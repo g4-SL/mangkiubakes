1f02086a1a2f8b493531bba60d3dc0fb3acae89a
g4-SL <slouise@live.com.my>
2015-09-14 15:15:59 +0800

    Updating some stuff

--- category.php
+++ category.php
@@ -25,7 +25,10 @@ get_header(); ?>
 					<div class="three columns" style="text-align:center;margin-bottom:10px">
 						<?php if(has_post_thumbnail()) {                    
 						    $image_src = wp_get_attachment_image_src( get_post_thumbnail_id(),'full' );
-						    echo '<img src="' . $image_src[0]  . '" width="100%"  />';
+						    ?>
+						    <a href="<?php the_permalink() ?>" title="Permanent Link to <?php the_title(); ?>">
+					    	<?php
+						    echo '<img src="' . $image_src[0]  . '" width="100%"  /></a>';
 						} ?>
 						<div style="width:80%;margin:0 10%">
 				        	<a href="<?php the_permalink() ?>" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a>
@@ -42,6 +45,8 @@ get_header(); ?>
 
 				<?php endwhile; ?>
 				</div>
+				
+				<?php twentyeleven_content_nav( 'nav-below' ); ?>
 
 			<?php else : ?>
 
--- front-page.php
+++ front-page.php
@@ -23,22 +23,38 @@ get_header(); ?>
 					<h1>What we do in the MangkiuBakes kitchen</h1>
 				        <div class="three columns">
 			            	<h2>Fondant Cakes</h2>
-			            	<img src="http://localhost/wp/wp-content/uploads/2015/08/fondant.jpg" alt="" class="intro-cat">
+    						<?php
+    							$category_id = get_cat_ID( 'fondant' );
+						    	$category_link = get_category_link( $category_id );
+						    ?>
+						    <a href="<?php echo esc_url( $category_link ); ?>" title="Fondant Cakes"><img src="http://localhost/wp/wp-content/uploads/2015/08/fondant.jpg" alt="" class="intro-cat"></a>
 			            	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et</p>
 			            </div>
 				        <div class="three columns">
 			            	<h2>Cupcakes</h2>
-			            	<img src="http://localhost/wp/wp-content/uploads/2015/08/cupcakes.jpg" alt="" class="intro-cat">
+    						<?php
+    							$category_id = get_cat_ID( 'cupcakes' );
+						    	$category_link = get_category_link( $category_id );
+						    ?>
+						    <a href="<?php echo esc_url( $category_link ); ?>" title="Cupcakes"><img src="http://localhost/wp/wp-content/uploads/2015/08/cupcakes.jpg" alt="" class="intro-cat"></a>
 			            	<p>dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip</p>
 			            </div>
 				        <div class="three columns">
 			            	<h2>Cookies</h2>
-			            	<img src="http://localhost/wp/wp-content/uploads/2015/08/cookies.jpg" alt="" class="intro-cat">
+    						<?php
+    							$category_id = get_cat_ID( 'cookies' );
+						    	$category_link = get_category_link( $category_id );
+						    ?>
+						    <a href="<?php echo esc_url( $category_link ); ?>" title="Fondant Cakes"><img src="http://localhost/wp/wp-content/uploads/2015/08/cookies.jpg" alt="" class="intro-cat"></a>
 			            	<p>ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu</p>
 			            </div>
 				        <div class="three columns">
 			            	<h2>Cakes</h2>
-			            	<img src="http://localhost/wp/wp-content/uploads/2015/08/cakes.jpg" alt="" class="intro-cat">
+    						<?php
+    							$category_id = get_cat_ID( 'cakes' );
+						    	$category_link = get_category_link( $category_id );
+						    ?>
+						    <a href="<?php echo esc_url( $category_link ); ?>" title="Fondant Cakes"><img src="http://localhost/wp/wp-content/uploads/2015/08/cakes.jpg" alt="" class="intro-cat"></a>
 			            	<p>fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt</p>
 			            </div>
 					</div>
@@ -59,7 +75,7 @@ get_header(); ?>
 					<div style="text-align:center">
 				        <div class="three columns">
 				        	<h2>Policies</h2>
-			            	<img src="http://localhost/wp/wp-content/uploads/2015/08/rainbow-cake.jpg" alt="" class="intro-cat">
+			            	<a href="<?php echo esc_url(get_page_link(get_page_by_title('Policies')->ID)) ?>"><img src="http://localhost/wp/wp-content/uploads/2015/08/rainbow-cake.jpg" alt="" class="intro-cat"></a>
 			            	<p>fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt</p>
 			            </div>
 				        <div class="three columns">
