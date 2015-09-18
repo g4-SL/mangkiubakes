1849d63ef244fb353ace4d7d1a7c4e8295fbb881
g4-SL <slouise@live.com.my>
2015-09-18 21:35:44 +0800

    Added related post thumbnail for singles.php

--- single.php
+++ single.php
@@ -13,26 +13,31 @@ get_header(); ?>
 			<div id="content" role="main">
 
 				<div class="container">
-				<?php while ( have_posts() ) : the_post(); ?>
+					<?php while ( have_posts() ) : the_post(); ?>
 
-					<?php get_template_part( 'content-single', get_post_format() ); ?>
+						<?php get_template_part( 'content-single', get_post_format() ); ?>
 
-				<?php endwhile; // end of the loop. ?>
+					<?php endwhile; // end of the loop. ?>
 
+					<?php
+					$related = get_posts( array( 'category__in' => wp_get_post_categories($post->ID), 'numberposts' => 4, 'post__not_in' => array($post->ID) ) );
+					if( $related ) foreach( $related as $post ) {
+					setup_postdata($post); ?>
+
+			    	<div class="three columns space" style="text-align:center;margin-bottom:10px">   
+						<?php 
+					    $image_src = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' );
+					    ?>
+					    <a href="<?php the_permalink() ?>" title="Permanent Link to <?php the_title(); ?>">
+				    	<?php
+					    echo '<img src="' . $image_src[0]  . '" width="100%"  /></a>'; ?>
+						<div style="width:80%;margin:0 10%">
+				        	<a href="<?php the_permalink() ?>" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a>
+				        </div>
+					</div>
+					<?php }
+					wp_reset_postdata(); ?>
 				</div>
-
-				<?php
-
-				$related = get_posts( array( 'category__in' => wp_get_post_categories($post->ID), 'numberposts' => 5, 'post__not_in' => array($post->ID) ) );
-				if( $related ) foreach( $related as $post ) {
-				setup_postdata($post); ?>
-				 <ul> 
-			        <li>
-			        <a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-			        </li>
-			    </ul>   
-				<?php }
-				wp_reset_postdata(); ?>
 
 			</div><!-- #content -->
 		</div><!-- #primary -->
